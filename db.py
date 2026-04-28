df9007727649201fe69d5809fea9fba2f4995bdb
ToqaGhozlan <147687725+ToqaGhozlan@users.noreply.github.com>
2026-04-28 09:46:44 +0300

    Update db.py

--- db.py
+++ db.py
@@ -13,20 +13,27 @@ def init_db():
     conn = get_conn()
     cur = conn.cursor()
 
+    for i in range(1, 6):
+        cur.execute(f"""
+        CREATE TABLE IF NOT EXISTS tm_part{i} (
+            id SERIAL PRIMARY KEY,
+            leg_name TEXT,
+            leg_number TEXT,
+            mg_num TEXT,
+            mg_page TEXT,
+            year TEXT,
+            mod_articles JSONB
+        );
+        """)
+    cur.execute(f"ALTER TABLE tm_part{i} ADD COLUMN IF NOT EXISTS year TEXT;")
     cur.execute("""
-    CREATE TABLE IF NOT EXISTS tm_part1 (
+    CREATE TABLE IF NOT EXISTS user_progress (
         id SERIAL PRIMARY KEY,
-        leg_name TEXT,
-        leg_number TEXT,
-        mg_num TEXT,
-        mg_page TEXT,
-        year TEXT,
-        mod_articles JSONB
+        username TEXT UNIQUE,
+        law_idx INTEGER DEFAULT 0,
+        mod_idx INTEGER DEFAULT 1
     );
     """)
-
-    # ضمان وجود العمود في الجداول القديمة
-    cur.execute("ALTER TABLE tm_part1 ADD COLUMN IF NOT EXISTS year TEXT;")
 
     conn.commit()
     cur.close()
